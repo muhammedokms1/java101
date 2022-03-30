b5c1484d031670e83074853c82f32443ae702f23
Muhammet <102230673+muhammedokms1@users.noreply.github.com>
2022-03-30 22:05:38 +0300

    Add files via upload

---
+++ kullaniciGirisi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class kullaniciGirisi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userName,password;
+
+        System.out.print("Kullanıcı adınızı giriniz : ");
+        userName = input.nextLine();
+        System.out.print("Şifrenizi giriniz : ");
+        password = input.nextLine();
+
+        if(userName.equals("patika")&& password.equals("java123") ){
+            System.out.println("Giriş yaptınız.");
+
+
+        }else{
+            System.out.println("Bilgileriniz Yanlış !");
+        }
+
+
+    }
+}
